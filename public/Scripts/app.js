25209aa3116e4735f3d57fa14eb32c0436296241
Manthan Shah <manthanshah1711@gmail.com>
2019-01-26 12:33:18 -0800

    added icons

---
+++ public/Scripts/app.js
@@ -0,0 +1,7 @@
+//IIFE
+(function(){
+function Start(){
+    console.log(`%c App Started...`, "font-size: 20px; color: blue; font-weight:bold")
+}
+window.addEventListener("load",Start);
+})();
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
 });
+
+router.get('about/', (req, res, next) => {
+  res.render('index', { title: 'About' });
+});
+
+router.get('contact/', (req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('product/', (req, res, next) => {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('services/', (req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
 
 module.exports = router;
