55a05a25f2a5a75cf0fce73f5600295f9db19b65
Manthan Shah <manthanshah1711@gmail.com>
2019-02-02 12:23:30 -0800

    added mongo support

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     //local mongoDB deployment
-    "URI":"mongodb://localhost/demo"
+    //"URI":"mongodb://localhost/demo"
+    "URI":"mongodb://<dbuser>:<dbpassword>@ds060977.mlab.com:60977/comp308-w2019-lesson4a"
 }
